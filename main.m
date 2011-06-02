eead11801a2553ba15a2b52b02e9f74584e9f368
woodenglider <glider21cn@gmail.com>
2011-06-02 14:36:00 +0800

    create project

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NearBy
+//
+//  Created by 任春宁 on 11-3-26.
+//  Copyright sursen 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+
